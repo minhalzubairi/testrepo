1334f2b87ab843b0d1ab94525902dad85d86e74c
minhalzubairi <81471473+minhalzubairi@users.noreply.github.com>
2024-10-25 11:12:35 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="title" content="Hacked By 4 EXPLOITATION">
+    <meta name="description" content="Security System Is Fucked Up">
+    <meta name="keywords" content="4 EXPLOITATION">
+    <meta name="image" content="https://i.ibb.co/LZTTJVt/photo-2023-03-07-17-47-46.jpg">
+    <link rel="icon" type="image/x-icon" href="https://i.ibb.co/LZTTJVt/photo-2023-03-07-17-47-46.jpg">
+    <meta name="googlebot" content="index,follow">
+    <meta name="robots" content="index, follow">
+    <meta name="robots schedule" content="auto">
+    <meta name="distribution" content="global">
+    <iframe src="https://k.top4top.io/m_2156ex9e51.mp3" allow="autoplay" style="display:none;" id="iframeAudio"></iframe>
+    <title>Hacked By 4 EXPLOITATION</title>
+   <script>
+ var source = "https://k.top4top.io/m_2156ex9e51.mp3"
+ var audio = document.createElement("audio");
+ //
+ audio.autoplay = true;
+ //
+ audio.load()
+ audio.addEventListener("load", function() { 
+     audio.play(); 
+ }, true);
+ audio.src = source;
+    </script>
+  <style>
+    html, body {
+      background-color: #FFFFFF;
+      height: 100vh;
+      margin: 0;
+    }
+    .full-height {
+      height: 100vh;
+    }
+    .flex-center {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+    .position-ref {
+      position: relative;
+    }
+    .content {
+      text-align: center;
+    }
+    .title {
+      font-size: 36px;
+      padding: 20px;
+    }
+    h {
+      color:#FF0000 ;
+    }
+    g {
+      color: green;
+    }
+  </style>
+  <body align="center" oncontextmenu="return false" onclick="play()">
+    <div class="flex-center position-ref full-height">
+      <div class="content">
+        <div class="text">
+          <img src="https://s1.bukalapak.com/img/66962014891/large/Bendera_Tauhid_Panji_Islam_Model_Hitam_Putih.png" height="225px">
+          <!--<img src="http://www.animated-gifs.fr/category_sports/sports-olympics-flags-boys/0112.gif" width="225px">-->
+          <br>
+          <font face="courier new" color="black">
+            <b>
+              Hacked By 4 EXPLOITATION X GhostTeam
+            <br>
+            <br>
+              We Are Ummah Hacker , We Are The Sound Of Palestine and We Will Not Remain Silent ! 
+  <br> We Didn't Sleep When You Start Killing Innocent Children And Family , We Didn't Sleep When You Start Destroy Their House And Residence 
+  <br> Death To All Jews
+  <br> We Stand With Palestine And Remember Our Name
+  <br> <h>#OpIsreal</h> Engaged !!
+            <br>
+            <br>
+              Thanks To : <br> {} <g>D$nnny</g> {} <g>Dr4zaxx</g> {} <g>Roti Encem</g> {} <g>LapanWasTaken</g>  
+            <br>
+            Greetz : <br>
+            #<g>AnonGhost</g>
+#<g>AnonGhostIndonesia</g> #<g>4-EXPLOITATION</g> #<g>GhostTeam</g> #<g>StuckxTeam</g> #<g>EagleCyber</g> #<g>SynixCyberCrime</g> #<g>TengkorakCyberCrew</g> #<g>stand4muslim</g> #<g>stand4Palestine</g> #<g>Th3WhiteCrew</g> 
+            </b>
+          </font>
+        </div>
+     </body>
+  </head>
+</html>
